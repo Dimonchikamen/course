945583cca5443ca90bf2b9fca79a4eaeea0c5a78
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2023-07-09 01:37:17 +0500

    feat: добавил роутинг и lazy компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import {FC} from "react";
+import {FC, Suspense} from "react";
 import {Counter} from "./components/Counter";
 import "./index.scss";
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 
 const App: FC = () => {
     return (
         <div className="app">
+            <Link to={"/about"}>Главная</Link>
+            <Link to={"/main"}>About</Link>
+            <Suspense fallback={<div>Loading</div>}>
+            <Routes>
+                    <Route path={"/about"} element={<AboutPageLazy />}/>
+                    <Route path={"/main"} element={<MainPageLazy />}/>
+            </Routes>
+            </Suspense>
             qweqweeqwewqee
             <Counter/>
         </div>
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import {FC} from "react";
+import {Link} from "react-router-dom";
+
+const AboutPage: FC = () => {
+    return (
+        <div>
+            <h1>
+                About Page
+            </h1>
+        </div>
+    )
+}
+
+export default AboutPage;
