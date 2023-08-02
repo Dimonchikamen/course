82681327e703ca56cfc134548a512df7898f6e86
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2023-08-02 11:29:47 +0500

    fix: fix path to svg Empty components

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     setupFilesAfterEnv: ["<rootDir>config/jest/jest.setup.ts"],
     verbose: true,
     moduleNameMapper: {
-        "\\.svg": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+        "\\.svg": path.resolve(__dirname, "JestEmptyComponent.tsx"),
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/__mocks__/fileMock.js",
         "\\.(css|less|scss)$": "identity-obj-proxy",
