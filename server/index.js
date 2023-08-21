905404aeefe3759cd910a5ee7c3a2e2487883a70
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2023-08-21 13:28:16 +0500

    fix(server): fix server responses

--- server/index.js
+++ server/index.js
@@ -5,12 +5,32 @@ const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
 server.use(async (req, res, next) => {
     await new Promise(res => {
-        const randomMs = Math.max(0.5, Math.random()) * 100;
+        const randomMs = Math.max(700, Math.random() * 1000);
         setTimeout(res, randomMs);
     });
     next();
+});
+
+server.post("/login", (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const users = JSON.parse(fs.readFileSync(path.resolve(__dirname, "users.json"), { encoding: "utf-8" }));
+
+        const userFromDb = users.find(u => u.username === username && u.password === password);
+
+        if (userFromDb) {
+            return res.json(userFromDb);
+        }
+
+        return res.status(400).json({ message: "invalid login or password" });
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
 });
 
 server.use((req, res, next) => {
@@ -21,22 +41,6 @@ server.use((req, res, next) => {
     next();
 });
 
-server.use(jsonServer.defaults());
 server.use(router);
 
-server.post("/login", (req, res) => {
-    const { username, password } = req.body;
-    const db = JSON.parse(String(fs.readFileSync(path.resolve(__dirname, "users.json"), { encoding: "utf-8" })));
-    const { users } = db;
-    const userFromDb = users.find(u => u.username === username && u.password === password);
-
-    if (userFromDb) {
-        return res.json(userFromDb);
-    }
-
-    return res.status(400).json({ message: "invalid login or password" });
-});
-
-server.listen(5000, () => {
-    console.log("server start in 5000 port!");
-});
+server.listen(8000);
