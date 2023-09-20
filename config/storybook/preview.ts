15dd7ca54c3c88129442ccb3fb38eaa50558ff52
Dmitrii Chechikhin <65847130+Dimonchikamen@users.noreply.github.com>
2023-09-20 16:02:36 +0500

    fix(storybook): added route decorator

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,6 +1,7 @@
 import "loki/configure-react";
 import type { Preview } from "@storybook/react";
 import { Theme } from "../../src/app/providers/ThemeProvider";
+import { RouterDecorator } from "../../src/shared/config/storybook/RouterDecorator";
 import { StyleDecorator } from "../../src/shared/config/storybook/StyleDecorator";
 import { TranslationDecorator } from "../../src/shared/config/storybook/TranslationDecorator";
 import { ThemeDecorator } from "../../src/shared/config/storybook/ThemeDecorator";
@@ -16,7 +17,7 @@ const preview: Preview = {
             },
         },
     },
-    decorators: [StyleDecorator, TranslationDecorator, ThemeDecorator(Theme.light)],
+    decorators: [RouterDecorator, StyleDecorator, TranslationDecorator, ThemeDecorator(Theme.light)],
 };
 
 export default preview;
--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -3,7 +3,6 @@ import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
 import { loginReducer } from "features/AuthByUserame/model/slice/loginSlice";
 import { ReducersList } from "shared/lib/components";
 import { DeepPartial } from "shared/model/types";
-import { BrowserRouter } from "react-router-dom";
 
 const defaultReducers: ReducersList = {
     loginForm: loginReducer,
@@ -13,13 +12,11 @@ export const StoreDecorator =
     (initialState: DeepPartial<StateSchema>, asyncReducers?: ReducersList) =>
     (StoryComponent: () => ReturnType<StoryFn>) => {
         return (
-            <BrowserRouter>
-                <StoreProvider
-                    initialState={initialState}
-                    asyncReducers={{ ...defaultReducers, ...asyncReducers }}
-                >
-                    <StoryComponent />
-                </StoreProvider>
-            </BrowserRouter>
+            <StoreProvider
+                initialState={initialState}
+                asyncReducers={{ ...defaultReducers, ...asyncReducers }}
+            >
+                <StoryComponent />
+            </StoreProvider>
         );
     };
